de5bf5c58498fc14b4b4223685f986c30bf65f0b
Constantin Zackl <constantin.zackl@gmail.com>
2024-05-16 18:00:52 +0200

    got forecast time

--- main.js
+++ main.js
@@ -110,6 +110,12 @@ async function loadWind(url) {
             velocityType: "",
         }
     }).addTo(themaLayer.wind);
+
+
+    let forecastDate = new Date(jsondata[0].header.refTime);
+    forecastDate.setHours(forecastDate.getHours() + jsondata[0].header.forecastTime);
+    console.log(forecastDate.toLocaleString());
+
 }
 
 loadWind("https://geographie.uibk.ac.at/data/ecmwf/data/wind-10u-10v-europe.json")
